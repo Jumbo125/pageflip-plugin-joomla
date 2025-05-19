8e24d24d130c2a9e9f94fd133228c2b3c4705aa6
Jumbo125 <61667987+Jumbo125@users.noreply.github.com>
2025-05-19 23:56:32 +0200

    fix joomla plugin

--- services/provider.php
+++ services/provider.php
@@ -1,4 +1,21 @@
 <?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  [PLUGIN_NAME]
+ * @author      jumbo125
+ * @copyright   Copyright (C) 2025 jumbo125. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ *
+ * Fremde Skripte / Third-party libraries:
+ * - Original library: StPageFlip, Copyright (c) 2020 Nodlik, https://github.com/Nodlik/StPageFlip
+ * - Panzoom 4.6.0 for panning and zooming using CSS transforms, Copyright Timmy Willison and contributors
+ * - Bootstrap 4 with Bootstrap Icons
+ * - jQuery, jQuery UI
+ */
+
+// Sicherheitscheck
+defined('_JEXEC') or die;
+
 use Joomla\CMS\Extension\PluginInterface;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\PluginHelper;
